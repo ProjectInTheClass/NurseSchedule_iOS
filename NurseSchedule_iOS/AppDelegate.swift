16cd8509f383e2e6df49993bdb30c2c26bb3580a
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-01-05 14:06:32 +0900

    Update AppDelegate.swift

--- NurseSchedule_iOS/AppDelegate.swift
+++ NurseSchedule_iOS/AppDelegate.swift
@@ -8,27 +8,15 @@
 import UIKit
 import Firebase
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+//@UIApplicationMain
 
-  var window: UIWindow?
-
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
 
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
