f49de3c22639536f4eb2c30d965c5a88a5cfd820
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-01-05 14:12:02 +0900

    Update AppDelegate.swift

--- NurseSchedule_iOS/AppDelegate.swift
+++ NurseSchedule_iOS/AppDelegate.swift
@@ -8,10 +8,6 @@
 import UIKit
 import Firebase
 
-//@UIApplicationMain
-
-
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
