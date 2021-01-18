438ed9d7fad8b561729d1028263b6d7e3d17ebed
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2021-01-19 03:15:09 +0900

    [WIP] ScheduleAddViewController.swift
    outlets connected
    textfield added
    generating newMemo instance

--- NurseSchedule_iOS/NurseSchedule_iOS/Controllers/ScheduleAddViewController.swift
+++ NurseSchedule_iOS/NurseSchedule_iOS/Controllers/ScheduleAddViewController.swift
@@ -7,16 +7,66 @@
 
 import UIKit
 
-class ScheduleAddViewController: UIViewController {
+enum WorkType {
+    case DAY
+    case EVENING
+    case NIGHT
+    case OFF
+}
+
+struct NewMemo {
+    var date : String
+    var workType : WorkType
+    var memo : String
+}
+
+class ScheduleAddViewController: UIViewController{
 
     @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var workTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var memoTextField: UITextField!
+    
+    var newMemo = NewMemo(date: "default_date", workType: .DAY, memo: "default_memo_content")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //memoTextField.delegate = self
        
         // Do any additional setup after loading the view.
     }  
     
+    // 메모에 들어가는 최대 글자수 제한
+    func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if (memoTextField.text?.count ?? 0 > maxLength) {
+            memoTextField.deleteBackward()
+        }
+    }
 
+    
+    // memoTextField에 글자써질때마다 글자수 체크
+    @IBAction func textDidChanged(_ sender: Any) {
+        checkMaxLength(textField: memoTextField, maxLength: 30)
+    }
+    
+    // memoTextField의 글자가 모두 쳐졌을 때
+    @IBAction func textDidEnd(_ sender: Any) {
+        if let memoContent = memoTextField.text {
+            newMemo.memo = memoContent
+        }
+    }
+    
+    @IBAction func workTypeSeleted(_ sender: UISegmentedControl) {
+        if workTypeSegmentedControl.selectedSegmentIndex == 0 {
+            newMemo.workType = .DAY
+        } else if workTypeSegmentedControl.selectedSegmentIndex == 1 {
+            newMemo.workType = .EVENING
+        } else if workTypeSegmentedControl.selectedSegmentIndex == 2 {
+            newMemo.workType = .NIGHT
+        } else {
+            newMemo.workType = .OFF
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -28,3 +78,4 @@ class ScheduleAddViewController: UIViewController {
     */
 
 }
+
